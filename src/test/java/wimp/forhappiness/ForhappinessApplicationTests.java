294489bb534d237b89e998a3815a2c16c6bd12f4
danbi <leedanbiii@naver.com>
2023-12-07 13:30:37 +0900

    프로젝트 생성

---
+++ src/test/java/wimp/forhappiness/ForhappinessApplicationTests.java
@@ -0,0 +1,13 @@
+package wimp.forhappiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForhappinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
